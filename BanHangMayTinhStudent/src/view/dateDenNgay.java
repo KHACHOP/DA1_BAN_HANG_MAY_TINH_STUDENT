4538a4c39ec0aa46f68990b7fb1083007415d5b9
DoanGiaHuy <huydoangia1109@gmail.com>
2024-08-01 22:30:48 +0700

    new

---
+++ BanHangMayTinhStudent/src/view/dateDenNgay.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Dell
+ */
+class dateDenNgay {
+     static Date getDate() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
